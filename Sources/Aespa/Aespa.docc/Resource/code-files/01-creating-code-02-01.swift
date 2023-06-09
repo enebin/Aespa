a4db4b835ee4011804a7ff7f9a10b1c5cd5b8185
enebin <enebin777@gmail.com>
2023-06-09 22:23:15 +0900

    Add tutorials - on going

---
+++ Sources/Aespa/Aespa.docc/Resource/code-files/01-creating-code-02-01.swift
@@ -0,0 +1,21 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view to customize a sloth.
+*/
+
+import SwiftUI
+
+struct CustomizedSlothView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomizedSlothView()
+    }
+}
---
+++ Sources/Aespa/Aespa.docc/Resource/code-files/01-creating-code-02-03.swift
@@ -0,0 +1,23 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view to customize a sloth.
+*/
+
+import SwiftUI
+import SlothCreator
+
+struct CustomizedSlothView: View {
+    @State var sloth: Sloth
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomizedSlothView()
+    }
+}
