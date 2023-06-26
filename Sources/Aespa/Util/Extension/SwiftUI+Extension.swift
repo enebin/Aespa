3018bf260de4392b81195546e8e7388e9bfeb024
enebin <enebin777@gmail.com>
2023-06-26 22:13:22 +0900

    Improve zooming for interactive preview

--- Sources/Aespa/Util/Extension/SwiftUI+Extension.swift
+++ Sources/Aespa/Util/Extension/SwiftUI+Extension.swift
@@ -10,13 +10,22 @@ import SwiftUI
 import AVFoundation
 
 public extension AespaSession {
+    func preview(
+        gravity: AVLayerVideoGravity = .resizeAspectFill,
+        startPosition position: AVCaptureDevice.Position = .back,
+        preferredFocusMode mode: AVCaptureDevice.FocusMode = .continuousAutoFocus
+    ) -> some View {
+        let internalPreview = Preview(of: self, gravity: gravity)
+        return InteractivePreview(internalPreview, startPosition: position, preferredFocusMode: mode)
+    }
+    
     /// A `SwiftUI` `View` that you use to display video as it is being captured by an input device.
     ///
     /// - Parameter gravity: Define `AVLayerVideoGravity` for preview's orientation.
     ///     .resizeAspectFill` by default.
     ///
     /// - Returns: `some UIViewRepresentable` which can coordinate other `View` components
-    func preview(
+    func interactivePreview(
         gravity: AVLayerVideoGravity = .resizeAspectFill,
         startPosition position: AVCaptureDevice.Position = .front,
         preferredFocusMode mode: AVCaptureDevice.FocusMode = .continuousAutoFocus
@@ -31,8 +40,9 @@ public struct InteractivePreview: View {
     private let preferredFocusMode: AVCaptureDevice.FocusMode
     @State private var cameraPosition: AVCaptureDevice.Position
     
-    @GestureState private var magnification: CGFloat = 1.0
-    
+    @State private var previousZoomFactor: CGFloat = 1.0
+    @State private var currentZoomFactor: CGFloat = 1.0
+
     init(
         _ preview: Preview,
         startPosition: AVCaptureDevice.Position,
@@ -55,22 +65,36 @@ public struct InteractivePreview: View {
         session.currentFocusMode ?? preferredFocusMode
     }
     
+    var currentCameraPosition: AVCaptureDevice.Position {
+        session.currentCameraPosition ?? cameraPosition
+    }
+    
     public var body: some View {
+        let maxZoomFactor = session.maxZoomFactor ?? 1.0
+
         preview
+            .onTapGesture(count: 2) {
+                let nextPosition: AVCaptureDevice.Position = (currentCameraPosition == .back) ? .front : .back
+                session.setPosition(to: nextPosition)
+                cameraPosition = nextPosition
+            }
             .gesture(DragGesture(minimumDistance: 0)
                 .onChanged { value in
                     if currentFocusMode == .autoFocus {
                         session.setFocus(mode: .autoFocus, point: value.location)
                     }
                 })
-            .onTapGesture(count: 2) {
-                let nextPosition: AVCaptureDevice.Position = cameraPosition == .back ? .front : .back
-                session.setPosition(to: nextPosition)
-            }
             .gesture(MagnificationGesture()
-                .updating($magnification) { currentState, gestureState, _ in
-                    gestureState = currentState
-                    session.zoom(factor: gestureState)
+                .onChanged { (scale) in
+                    let videoZoomFactor = scale * previousZoomFactor
+                    if (videoZoomFactor <= maxZoomFactor) {
+                        let newZoomFactor = max(1.0, min(videoZoomFactor, maxZoomFactor))
+                        session.zoom(factor: newZoomFactor)
+                    }
+                }
+                .onEnded { (scale) in
+                    let videoZoomFactor = scale * previousZoomFactor
+                    previousZoomFactor = videoZoomFactor >= 1 ? videoZoomFactor : 1
                 }
             )
     }
@@ -87,7 +111,7 @@ struct Preview: UIViewControllerRepresentable {
     ) {
         self.gravity = gravity
         self.session = session
-        self.previewLayer = AVCaptureVideoPreviewLayer(session: session.avCaptureSession)
+        self.previewLayer = session.previewLayer
     }
 
     func makeUIViewController(context: Context) -> UIViewController {
